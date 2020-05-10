e3e9c5adc3ac085dafbfbf62f0e1ef8d7b44f7ce
adam-laird <adam.laird5452@gmail.com>
2020-05-09 20:01:54 -0400

    Updates

---
+++ precache-manifest.ca618415fa1a442beff3ddd4883ea849.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2378b6c094a1f372d311cb3138ef8d5a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "00c879844432bff808d0",
+    "url": "/static/css/main.c6146e9c.chunk.css"
+  },
+  {
+    "revision": "6311505eeed6fb2cd580",
+    "url": "/static/js/2.cb93a52a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.cb93a52a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00c879844432bff808d0",
+    "url": "/static/js/main.d48fa5e3.chunk.js"
+  },
+  {
+    "revision": "ece4227a975c06513fb8",
+    "url": "/static/js/runtime-main.2668811c.js"
+  }
+]);
--- src/Components/Accessories/Theme.js
+++
@@ -1,8 +0,0 @@
-const colors = {
-  black: "#000000",
-  white: "#ffffffBF",
-};
-export default {
-  topBarColor: colors.black,
-  containerColor: colors.white,
-};
