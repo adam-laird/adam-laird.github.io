f17a4fbaaa9998462fb95fef95e6d3f4cf6dd516
adam-laird <adam.laird5452@gmail.com>
2020-12-14 16:14:22 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.bb66ac8c5c5b30d020a6654e23422d93.js"
+  "/precache-manifest.92d181b9a37f7d41cb291e443e06b519.js"
 );
 
 self.addEventListener('message', (event) => {
